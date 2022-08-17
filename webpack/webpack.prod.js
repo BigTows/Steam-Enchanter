907ac7a22ff25e8b1904618621dd1ba5ffd05ff2
Alexander Chapchuk <BigTows@yandex.ru>
2022-08-17 07:59:08 +0300

    Add CI

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,5 +2,7 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+    ]
 });
