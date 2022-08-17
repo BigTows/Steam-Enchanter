40f23aa584f922ec78bfae63cb8d4bd634f59849
Alexander Chapchuk <BigTows@yandex.ru>
2022-08-17 16:37:41 +0300

    Remove unused features.

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,12 +1,11 @@
-const webpack = require("webpack");
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const srcDir = path.join(__dirname, "..", "src");
 
 module.exports = {
     entry: {
-      popup: path.join(srcDir, 'popup.tsx'),
-      options: path.join(srcDir, 'options.tsx'),
+      // popup: path.join(srcDir, 'popup.tsx'),
+      // options: path.join(srcDir, 'options.tsx'),
       background: path.join(srcDir, 'background.ts'),
       content_script: path.join(srcDir, 'content_script.ts'),
     },
