a863e55b0f6f2c53a3a69e493f7888e0906b22f8
Alexander Chapchuk <BigTows@yandex.ru>
2022-08-17 08:18:54 +0300

    Update CI flow.

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,8 +1,46 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const FileManagerPlugin = require("filemanager-webpack-plugin");
 
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
+        new FileManagerPlugin({
+            events: {
+                onEnd: {
+                    delete: ['./build'],
+                    archive: [
+                        {
+                            source: './dist', destination: `./build/${process.env.npm_package_name}-${process.env.npm_package_version}.zip`,
+                            format: 'zip',
+                            options: {
+                                gzip: true,
+                                gzipOptions: {
+                                    level: 1,
+                                },
+                                globOptions: {
+                                    // https://github.com/Yqnn/node-readdir-glob#options
+                                    dot: true,
+                                },
+                            },
+                        },
+                        {
+                            source: './src', destination: `./build/${process.env.npm_package_name}-${process.env.npm_package_version}-source.zip`,
+                            format: 'zip',
+                            options: {
+                                gzip: true,
+                                gzipOptions: {
+                                    level: 1,
+                                },
+                                globOptions: {
+                                    // https://github.com/Yqnn/node-readdir-glob#options
+                                    dot: true,
+                                },
+                            },
+                        },
+                    ],
+                },
+            },
+        }),
     ]
 });
